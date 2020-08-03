988c82f99323ba5e66d090cd1c874a4e64aa4f95
Venkatesh <Harshith.Venkatesh@CenturyLink.com>
2020-08-03 18:23:29 +0530

    Event Schema and Speaker dummy data added

---
+++ api/data/event-schema.js
@@ -0,0 +1,13 @@
+const events = [
+  {
+    eventId: "123",
+    status: "Previous",
+    title: "RDS Event 1",
+    startTime: "2020-08-01",
+    duration: 5400,
+    description: "Test",
+    speakers: ["srds001"],
+    attendees: ["ards001", "ards002", "ards003"],
+    joiningUrl: " https://lnkd.in/eeDMcWp",
+  },
+];
---
+++ api/data/speaker-data.js
@@ -0,0 +1,15 @@
+const speakers = [
+  {
+    speakerId: "srds001",
+    fullName: "RDS Speaker Test",
+    avatarUrl: "Test",
+    jobTitle: "Test",
+    companyUrl: "Test",
+    githubUrl: "Test",
+    linkedinUrl: "Test",
+    twitterUrl: "Test",
+    mediumUrl: "Test",
+    codepenUrl: "Test",
+    prevTalks: ["Test"],
+  },
+];
